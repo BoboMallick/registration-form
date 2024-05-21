03df1d1bb79a865d284de59819e09339985b5545
bobo mallick <mallickbobo@gmail.com>
2024-05-21 22:38:07 +0530

    Some problem in js file

---
+++ script.js
@@ -0,0 +1,12 @@
+let signUpBtn = document.querySelector('.signupbtn');
+let signInBtn = document.querySelector('.signinbtn');
+let nameField = document.querySelector('.namefield');
+let title = document.querySelectquerySelector('.title');
+let underLine = document.querySelector('.underline');
+
+signInBtn.addEventListener('click',()=>{
+    nameField.style.maxHeight = '0';
+    title.innerHTML = 'Sing in';
+    signUpBtn.classList.add('disable');
+    signInBtn.classList.remove('disable');
+});
